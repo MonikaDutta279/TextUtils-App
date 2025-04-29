831d37017c13b7417d6165acb2ba30fb0a64553e
MonikaDutta279 <modu21is@cmrit.ac.in>
2025-04-29 11:46:44 +0530

    Updates

---
+++ precache-manifest.8eebe660d2617dd15d2ab16ec93a8227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa55da004073d020b8d4979cc11a631",
+    "url": "/TextUtils-App/index.html"
+  },
+  {
+    "revision": "2dd86af52d31e9349506",
+    "url": "/TextUtils-App/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/TextUtils-App/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextUtils-App/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "2dd86af52d31e9349506",
+    "url": "/TextUtils-App/static/js/main.b8d4dea8.chunk.js"
+  },
+  {
+    "revision": "7ad0483659267776a01b",
+    "url": "/TextUtils-App/static/js/runtime~main.56cf26ae.js"
+  }
+]);
